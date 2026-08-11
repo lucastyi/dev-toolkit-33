b7e058d6629008203e1912cbd7441119c6f26b67
Lukas Tydén <lukefortin7@gmail.com>
2026-08-11 05:14:44 +0200

    add logger class reorganization

---
+++ logger.py
@@ -0,0 +1,34 @@
+import logging
+
+class Logger:
+    def __init__(self, name):
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(logging.DEBUG)
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
+
+    def debug(self, message):
+        self.logger.debug(message)
+
+    def info(self, message):
+        self.logger.info(message)
+
+    def warning(self, message):
+        self.logger.warning(message)
+
+    def error(self, message):
+        self.logger.error(message)
+
+    def critical(self, message):
+        self.logger.critical(message)
+
+    def set_level(self, level):
+        self.logger.setLevel(level)
+
+    def add_file_handler(self, file_name):
+        file_handler = logging.FileHandler(file_name)
+        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+        file_handler.setFormatter(formatter)
+        self.logger.addHandler(file_handler)
