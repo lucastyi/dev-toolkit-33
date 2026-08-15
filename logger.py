34c7ff2e0650e20c5bcefe97c01e482ea55464fa
Lukas Tydén <lukefortin7@gmail.com>
2026-08-15 09:16:38 +0200

    add rotating logger setup implementation

--- logger.py
+++ logger.py
@@ -1,25 +1,18 @@
 import logging
+from logging.handlers import RotatingFileHandler
 
-def setup_logger(name):
-    logger = logging.getLogger(name)
-    if not logger.hasHandlers():
-        handler = logging.StreamHandler()
-        formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-        handler.setFormatter(formatter)
-        logger.addHandler(handler)
-        logger.setLevel(logging.DEBUG)
+def setup_logger(log_file, max_bytes=5*1024*1024, backup_count=3):
+    logger = logging.getLogger("dev_toolkit_logger")
+    logger.setLevel(logging.DEBUG)
+
+    handler = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+
+    logger.addHandler(handler)
+
     return logger
 
-logger = setup_logger('AutoClicker')
-
-def log_info(message):
-    logger.info(message)
-
-def log_warning(message):
-    logger.warning(message)
-
-def log_error(message):
-    logger.error(message)
-
-def log_debug(message):
-    logger.debug(message)
+if __name__ == '__main__':
+    log = setup_logger('app.log')
+    log.info('Logger setup complete')
