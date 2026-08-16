d80d3d6c25381841f04f768be80b539905485c01
Lukas Tydén <lukefortin7@gmail.com>
2026-08-16 09:15:08 +0200

    type annotations and docstrings for logger

--- logger.py
+++ logger.py
@@ -1,18 +1,28 @@
+# logger.py
 import logging
-from logging.handlers import RotatingFileHandler
 
-def setup_logger(log_file, max_bytes=5*1024*1024, backup_count=3):
-    logger = logging.getLogger("dev_toolkit_logger")
+# Configure the logger
+def configure_logger(name: str) -> logging.Logger:
+    """
+    Configure a logger with the specified name.
+    
+    Args:
+        name: The name of the logger. Used for distinguishing different loggers.
+    
+    Returns:
+        A configured logger instance.
+    """
+    logger = logging.getLogger(name)
     logger.setLevel(logging.DEBUG)
-
-    handler = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
+    handler = logging.StreamHandler()
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
-
     logger.addHandler(handler)
-
     return logger
 
+# Example usage
 if __name__ == '__main__':
-    log = setup_logger('app.log')
-    log.info('Logger setup complete')
+    logger = configure_logger('AutoClicker')
+    logger.info('Logger is configured and ready.')
+    logger.debug('This is a debug message.')
+    logger.error('This is an error message.')
