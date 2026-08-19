d7be41e50c5fbaf36daf493c7fb9153a6fa50a81
Lukas Tydén <lukefortin7@gmail.com>
2026-08-19 09:19:45 +0200

    input validation in main processing loop

--- processor.py
+++ processor.py
@@ -1,41 +1,28 @@
 import time
 import random
 
-class Clicker:
-    def __init__(self, click_delay=0.1):
-        self.click_delay = click_delay
-        self.running = False
+def validate_input(value):
+    if not isinstance(value, int) or value < 0:
+        raise ValueError('Input must be a non-negative integer')
+
+class AutoClicker:
+    def __init__(self, interval, clicks):
+        validate_input(interval)
+        validate_input(clicks)
+        self.interval = interval
+        self.clicks = clicks
 
     def start(self):
-        self.running = True
-        self._process_loop()
+        print('Starting autoclicker...')
+        for _ in range(self.clicks):
+            self.click()
+            time.sleep(self.interval)
 
-    def stop(self):
-        self.running = False
-
-    def _process_loop(self):
-        while self.running:
-            position = self.get_click_position()
-            if self.validate_position(position):
-                self.simulate_click(position)
-                time.sleep(self.click_delay)
-            else:
-                print('Invalid click position: {}'.format(position))
-
-    def get_click_position(self):
-        # Simulate obtaining a random position
-        return (random.randint(0, 1920), random.randint(0, 1080))
-
-    def validate_position(self, position):
-        x, y = position
-        return isinstance(x, int) and isinstance(y, int) and 0 <= x <= 1920 and 0 <= y <= 1080
-
-    def simulate_click(self, position):
-        print(f'Simulated click at {position}')
+    def click(self):
+        print('Click!')  # Simulate a click
 
 if __name__ == '__main__':
-    clicker = Clicker(click_delay=0.5)
-    try:
-        clicker.start()
-    except KeyboardInterrupt:
-        clicker.stop()  # Ensures graceful stop after an interrupt
+    click_interval = random.randint(1, 3)
+    total_clicks = random.randint(5, 10)
+    autoclicker = AutoClicker(click_interval, total_clicks)
+    autoclicker.start()
